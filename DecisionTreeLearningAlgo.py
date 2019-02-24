3763beee77b6cf44e70e27e08f8a48d9d1b4b96f
Kedar Ashok Kulkarni <39630740+kedarkulk7@users.noreply.github.com>
2019-02-24 01:11:24 -0600

    Update DecisionTreeLearningAlgo.py

--- DecisionTreeLearningAlgo.py
+++ DecisionTreeLearningAlgo.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
 """
-@author: Kedar Kulkarni
+Created on Mon Dec 24 23:12:45 2018
+
+@author: kedar
 """
 import math
 import pandas as pd
 import random
 import datetime as dt
+import copy
+import sys
 
 class Node:
     def __init__(self):
@@ -13,9 +17,9 @@ class Node:
         self.name=''
         self.gain=''
         self.cnodes = []
-    
+
 def getEntropy(data, method,distCount):
-    list = data.groupby(data.iloc[:,-1]).count().iloc[:,-1]   
+    list = data.groupby(data.iloc[:,-1]).count().iloc[:,-1]
     ep = 0
     count=0
     if method == 'Variance':
@@ -31,7 +35,7 @@ def getEntropy(data, method,distCount):
     return ep,list.index[0]
 
 def getVariance(data):
-    list = data.groupby(data.iloc[:,-1]).count().iloc[:,-1]   
+    list = data.groupby(data.iloc[:,-1]).count().iloc[:,-1]
     ep = 1
     for i,a in enumerate(list):
         ep*= (a/sum(list))
@@ -41,37 +45,37 @@ def getGain(data,x,method,distCount):
       m = data.groupby(data.columns[x])
       e2=0
       total = sum(m.count().iloc[:,-1])
-      for a,b in m:          
+      for a,b in m:
           cnt = (b.count().iloc[-1])
           ent = getEntropy(b,method,distCount)
           e2 += (cnt/total)*ent[0]
-      return e2,ent[1]    
-    
+      return e2,ent[1]
+
 def findBestAttribute(data,e1,method,distCount):
     l = []
     map = {}
     for i,column in enumerate(data):
-       if column!=data.columns[-1]: 
+       if column!=data.columns[-1]:
         ent = getGain(data,i,method,distCount)
         gain = e1-ent[0]
         l.append(gain)
         map[gain]=column
-    n = Node()    
+    n = Node()
     if not l:
         n.name = 0
         n.gain = 0
-    else:    
+    else:
         n.gain = max(l)
         n.name = map.get(max(l))
     return n
 
-def getNode(data,e1,method,distCount):        
+def getNode(data,e1,method,distCount):
         n = findBestAttribute(data,e1,method,distCount)
         cat = data.groupby(n.name)
         data,n,dump=getChilds(cat,n,' ',method,distCount)
         return n
-        
-def getChilds(cat,nn,s,method,distCount):        
+
+def getChilds(cat,nn,s,method,distCount):
         for c,newdata in cat:
             nx = Node()
             data = newdata.drop(nn.name,axis=1)
@@ -86,19 +90,24 @@ def getChilds(cat,nn,s,method,distCount):
                 nx = getChilds(data.groupby(nx.name),nx,s+' ',method,distCount)[1]
             nn.cnodes.append(nx)
         return data,nn,s
-        
-def printTree(s,node,nfnc):
+
+def printTree(s,node,nfnc,yn):
+  if yn=='yes':
     for nd in node.cnodes:
         print(s+node.name+' = '+str(nd.connection)+' : ',end=''),
         if not nd.cnodes:
             print(str(nd.name))
         else:
             print()
-            nfnc+=1
-            nfnc = printTree(s+' ',nd,nfnc)            
-    return nfnc
-            
-def test(row,node):   
+            printTree(s+' ',nd,nfnc,yn)
+  else:
+    for nd in node.cnodes:
+            nfnc=nfnc+1
+            nfnc = printTree(s+' ',nd,nfnc,yn)
+
+    return int(nfnc)
+
+def test(row,node):
     val=row[node.name]
     if not node.cnodes:
         return node
@@ -108,7 +117,7 @@ def test(row,node):
                 a=test(row,a)
                 return a
 
-def prunedNode(bn,cnt,pk,fd):    
+def prunedNode(bn,cnt,pk,fd):
     for a in bn.cnodes:
         for ca in a.cnodes:
             cnt+=1
@@ -122,7 +131,7 @@ def prunedNode(bn,cnt,pk,fd):
                         break
             elif cnt<pk:
                 a=prunedNode(a,cnt,pk,fd)
-    return bn                
+    return bn
 
 def validate(vdata,node):
     rightcount=0
@@ -147,47 +156,61 @@ def mainmethod(data,vdata,method,l,k):
  else:
      dd = data
      node=getNode(dd,e1,method,distCount)
-     print('Printing decision tree')
-     nfnc=printTree('',node,nfnc)
+     nfnc=printTree('',node,nfnc,'no')
  t2=dt.datetime.now()
  print('Training time :',t2-t1,'sec')
- print('Validating')
  iaccu=validate(vdata,node)
  t3=dt.datetime.now()
  print('Validating time :',t3-t2,'sec')
- print('Pruning')
 
- bn = node 
+ bn = Node()
+ inbn=Node()
+ prbn=Node()
+ bn = copy.deepcopy(node)
+ inbn=copy.deepcopy(bn)
+ prbn=copy.deepcopy(inbn)
+
  for ll in range(l):
      m = random.randint(1,k)
      for mm in range(m):
          pk=random.randint(2,nfnc)
          cnt = 1
-         pn=prunedNode(bn,cnt,pk,data)
+         pn=prunedNode(inbn,cnt,pk,vdata)
          iac=validate(vdata,pn)
          if iac>iaccu:
              iaccu = iac
-             bn=pn
+             inbn=copy.deepcopy(pn)
+             prbn=copy.deepcopy(inbn)
              nfnc=1
-             nfnc=printTree('',bn,nfnc)
+             nfnc=printTree('',bn,nfnc,'n')
+         else:
+           inbn = copy.deepcopy(prbn)
+
+
  t4=dt.datetime.now()
  print('Pruning time :',t4-t3,'sec')
  print('Maximum accuracy achieved in',method,iaccu,'%')
+ return inbn
 
-data = input('Enter path for training set: ')
-vdata = input('Enter path for test set: ')
+l = int(sys.argv[1])
+k = int(sys.argv[2])
+data = pd.read_csv(str(sys.argv[3]))
+vdata = pd.read_csv(str(sys.argv[4]))
+tdata = pd.read_csv(str(sys.argv[5]))
+yn = str(sys.argv[6]).lower()
 
-if data=='':
-    data = pd.read_csv("E:/ML_A1_D1/data_sets1/training_set.csv")
-if vdata=='':
-    vdata = pd.read_csv("E:/ML_A1_D1/data_sets1/validation_set.csv")
-l = input('Enter value of L: ')
-k = input('Enter value of K: ')
-if l=='':
-    l=10
-if k =='':
-    k=15
-print('Entropy')
-mainmethod(data,vdata,'Entropy',int(l),int(k))
-print('Variance')
-mainmethod(data,vdata,'Variance',int(l),int(k))
+print('----------------------------Information Gain------------------------------------')
+bn = mainmethod(data,vdata,'Entropy',int(l),int(k))
+if yn=='yes':
+  print('Decision Tree')
+  printTree('',bn,int(0),yn)
+iaccu=validate(tdata,bn)
+print('Final accuracy for test set is',iaccu,'%')
+
+print('------------------------------------Variance Impurity----------------------------------')
+bn = mainmethod(data,vdata,'Variance',int(l),int(k))
+if yn=='yes':
+  print('Decision Tree')
+  printTree('',bn,int(0),yn)
+iaccu=validate(tdata,bn)
+print('Final accuracy for test set is',iaccu,'%')
